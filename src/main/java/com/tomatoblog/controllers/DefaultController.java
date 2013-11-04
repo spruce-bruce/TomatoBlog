ffa69cff01198dc9975593f532b813d292b0b72b
Aaron Bruce <aarondbruce@gmail.com>
2013-11-03 22:49:34 -0700

    cleaning up the default controller

--- src/main/java/com/tomatoblog/controllers/DefaultController.java
+++ src/main/java/com/tomatoblog/controllers/DefaultController.java
@@ -1,10 +1,7 @@
 package com.tomatoblog.controllers;
 
-import com.tomatoblog.dao.JdbcPostDAOImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,22 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class DefaultController {
-    @Autowired
-    JdbcPostDAOImpl postDAO;
-
     @RequestMapping(value="/", method=RequestMethod.GET)
     public String index(ModelMap map){
-        //JdbcPostDAOImpl postDAO = new JdbcPostDAOImpl();
-        int count = postDAO.getPostCount();
-        map.addAttribute("hello", Integer.toString(count));
+        map.addAttribute("hello", "test run");
         return "/index";
     }
-    
-    // @RequestMapping(value="/viewdemo")
-    // public String demo(@ModelAttribute("SpringWeb") Person person, ModelMap map){
-    //     map.put("personObject", person);
-    //     map.addAttribute("helloAgain", "Hello (again) spring from NB");
-        
-    //     return "demo";
-    // }
 }
